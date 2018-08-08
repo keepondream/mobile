1d572d686790fbe468e6ad74ea30e98cbd721b70
keepondream <402314889@qq.com>
2018-08-09 00:50:49 +0800

    增加guzzle扩展

--- app/Libraries/Sms51ym/Sms51ym.php
+++ app/Libraries/Sms51ym/Sms51ym.php
@@ -8,10 +8,39 @@
 
 namespace App\Libraries\Sms51ym;
 
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Cache;
 
 class Sms51ym
 {
+    private $password = '198522';
+    private $username = 'Fanleguan';
+    private $token = '';
 
+    protected $client = '';
+
+    public function __construct()
+    {
+        $this->client = new Client();
+
+        $token = Cache::get('sms51ymtoken');
+        if (empty($token)) {
+            dd($this->Token());
+        } else {
+            $this->token = $token;
+        }
+        dd($token);
+    }
+
+    public function Token()
+    {
+        $response = $this->client->request('GET', 'http://api.fxhyd.cn/UserInterface.aspx?action=login&username='.$this->username.'&password='.$this->password);
+        var_dump($response->getStatusCode());
+        var_dump($response->getHeaderLine('content-type'));
+        var_dump($response->getBody()->getContents());
+        exit;
+        dd($response->getBody());
+    }
 
     public static function actionIndex()
     {
